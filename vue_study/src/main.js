398dd418d1e19936518ced4f1f9ebce6ea143c28
Yuhj <294882560@qq.com>
2022-06-09 18:24:51 +0800

    一点修改

--- vue_study/src/main.js
+++ vue_study/src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: yuhj
+ * @Date: 2022-05-30 09:11:31
+ */
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  // 安装全局事件总线
+  beforeCreate() {
+    Vue.prototype.$bus = this;
+  },
+}).$mount('#app');
